b707223bcb0f1aa6e541b1737ba8fe880d4ee51d
Septemberer <tolik.laptev.02@mail.ru>
2022-06-30 20:28:24 +0300

    ; in JS added

--- test/dfERC20.test.js
+++ test/dfERC20.test.js
@@ -1,4 +1,4 @@
-const { expect } = require("chai")
+const { expect } = require("chai");
 const { utils } = require("ethers");
 const { ethers } = require("hardhat")
 const { BigNumber } = require("ethers");
@@ -25,51 +25,51 @@ describe("dfERC20", function () {
   let dev2;
 
   beforeEach(async function () {
-    [alice, dev2, dev, minter] = await ethers.getSigners()
-    const Token = await ethers.getContractFactory("LToken", minter)
-    const DToken = await ethers.getContractFactory("dfERC20", dev2)
-    const WETH = await ethers.getContractFactory("WETH")
-    const PancakeFactory = await ethers.getContractFactory("PancakeFactory")
-    const PancakeRouter = await ethers.getContractFactory("PancakeRouter")
+    [alice, dev2, dev, minter] = await ethers.getSigners();
+    const Token = await ethers.getContractFactory("LToken", minter);
+    const DToken = await ethers.getContractFactory("dfERC20", dev2);
+    const WETH = await ethers.getContractFactory("WETH");
+    const PancakeFactory = await ethers.getContractFactory("PancakeFactory");
+    const PancakeRouter = await ethers.getContractFactory("PancakeRouter");
 
-    weth = await WETH.deploy()
-    await weth.connect(dev2).deployed()
+    weth = await WETH.deploy();
+    await weth.connect(dev2).deployed();
 
-    factory = await PancakeFactory.deploy(dev2.address)
-    await factory.connect(dev2).deployed()
+    factory = await PancakeFactory.deploy(dev2.address);
+    await factory.connect(dev2).deployed();
 
-    router = await PancakeRouter.deploy(factory.address, weth.address)
-    await router.connect(dev2).deployed()
+    router = await PancakeRouter.deploy(factory.address, weth.address);
+    await router.connect(dev2).deployed();
 
 
-    token = await Token.deploy(ONE_TOKEN.mul(100000))
-    dtoken = await DToken.deploy(token.address, router.address)
+    token = await Token.deploy(ONE_TOKEN.mul(100000));
+    dtoken = await DToken.deploy(token.address, router.address);
 
-    await token.deployed()
-    await dtoken.deployed()
+    await token.deployed();
+    await dtoken.deployed();
 
-    await token.connect(minter).transfer(dev2.address, ONE_TOKEN.mul(2000))
+    await token.connect(minter).transfer(dev2.address, ONE_TOKEN.mul(2000));
 
-    await token.connect(dev2).transfer(dtoken.address, ONE_TOKEN.mul(1000))
-    await dtoken.connect(dev2).transfer(dtoken.address, ONE_TOKEN.mul(1000))
+    await token.connect(dev2).transfer(dtoken.address, ONE_TOKEN.mul(1000));
+    await dtoken.connect(dev2).transfer(dtoken.address, ONE_TOKEN.mul(1000));
 
     // await dtoken.connect(dev2).startLiquidity(ONE_TOKEN.mul(100), ONE_TOKEN.mul(100));
-  })
+  });
 
   it("Should be deployed", async function () {
-    expect(dtoken.address).to.be.properAddress
-  })
+    expect(dtoken.address).to.be.properAddress;
+  });
 
   it("Test small transfer (without liquidity)", async function () {
-    await dtoken.connect(dev2).transfer(alice.address, ONE_TOKEN.mul(2))
-    expect(await dtoken.balanceOf(alice.address)).to.be.equal(ONE_TOKEN.mul(2))
-  })
+    await dtoken.connect(dev2).transfer(alice.address, ONE_TOKEN.mul(2));
+    expect(await dtoken.balanceOf(alice.address)).to.be.equal(ONE_TOKEN.mul(2));
+  });
 
   it("Test big transfer (with liquidity)", async function () {
     let start_bal = await dtoken.totalSupply();
 
-    await token.connect(dev2).approve(router.address, ONE_TOKEN.mul(100))
-    await dtoken.connect(dev2).approve(router.address, ONE_TOKEN.mul(100))
+    await token.connect(dev2).approve(router.address, ONE_TOKEN.mul(100));
+    await dtoken.connect(dev2).approve(router.address, ONE_TOKEN.mul(100));
 
     await router.connect(dev2).addLiquidity(
       token.address,
@@ -82,50 +82,50 @@ describe("dfERC20", function () {
       9999999999
     );
 
-    //console.log("Ручное добавление ликвидности работает")
+    //console.log("Ручное добавление ликвидности работает");
 
     await dtoken.connect(dev2).setLandS(ONE_TOKEN.mul(100));
 
-    //console.log("LandS установлен")
+    //console.log("LandS установлен");
 
-    await dtoken.connect(dev2).transfer(alice.address, ONE_TOKEN.mul(100))
-    await dtoken.connect(dev2).transfer(dev.address, ONE_TOKEN.mul(1000))
+    await dtoken.connect(dev2).transfer(alice.address, ONE_TOKEN.mul(100));
+    await dtoken.connect(dev2).transfer(dev.address, ONE_TOKEN.mul(1000));
 
-    await dtoken.connect(dev2).transfer(dev.address, ONE_TOKEN.mul(1000))
-    await dtoken.connect(dev2).transfer(dev.address, ONE_TOKEN.mul(1000))
+    await dtoken.connect(dev2).transfer(dev.address, ONE_TOKEN.mul(1000));
+    await dtoken.connect(dev2).transfer(dev.address, ONE_TOKEN.mul(1000));
 
-    await dtoken.connect(dev2).transfer(dev.address, ONE_TOKEN.mul(1000))
-    await dtoken.connect(dev2).transfer(dev.address, ONE_TOKEN.mul(1000))
-    await dtoken.connect(dev2).transfer(dev.address, ONE_TOKEN.mul(1000))
+    await dtoken.connect(dev2).transfer(dev.address, ONE_TOKEN.mul(1000));
+    await dtoken.connect(dev2).transfer(dev.address, ONE_TOKEN.mul(1000));
+    await dtoken.connect(dev2).transfer(dev.address, ONE_TOKEN.mul(1000));
 
-    expect(await dtoken.balanceOf(alice.address)).to.be.equal(ONE_TOKEN.mul(100))
-    expect(await dtoken.balanceOf(dev.address)).to.be.equal(ONE_TOKEN.mul(6000))
+    expect(await dtoken.balanceOf(alice.address)).to.be.equal(ONE_TOKEN.mul(100));
+    expect(await dtoken.balanceOf(dev.address)).to.be.equal(ONE_TOKEN.mul(6000));
 
-    //console.log("Переводы от специальных адресов проводятся без комиссии")
+    //console.log("Переводы от специальных адресов проводятся без комиссии");
 
-    await dtoken.connect(dev).transfer(alice.address, ONE_TOKEN.mul(5000))
+    await dtoken.connect(dev).transfer(alice.address, ONE_TOKEN.mul(5000));
 
-    expect(await dtoken.balanceOf(alice.address)).to.be.equal(ONE_TOKEN.mul(5000).mul(95).div(100).add(ONE_TOKEN.mul(100)))
+    expect(await dtoken.balanceOf(alice.address)).to.be.equal(ONE_TOKEN.mul(5000).mul(95).div(100).add(ONE_TOKEN.mul(100)));
 
-    expect(await dtoken.balanceOf(dev.address)).to.be.equal(ONE_TOKEN.mul(1000))
+    expect(await dtoken.balanceOf(dev.address)).to.be.equal(ONE_TOKEN.mul(1000));
 
-    //console.log("Переводы от других адресов проводятся с комиссией")
+    //console.log("Переводы от других адресов проводятся с комиссией");
 
     let end_bal = await dtoken.totalSupply();
 
     expect(end_bal < start_bal).to.be.true;
 
-    //console.log("totalSupply уменьшился")
+    //console.log("totalSupply уменьшился");
 
-  })
+  });
 
   it("Try to buy and sell", async function () {
 
     await dtoken.connect(dev2).setLandS(ONE_TOKEN.mul(100));
 
-    //console.log("try to buy")
-    await token.connect(dev2).transfer(alice.address, ONE_TOKEN.mul(100))
-    await token.connect(alice).approve(router.address, ONE_TOKEN.mul(100))
+    //console.log("try to buy");
+    await token.connect(dev2).transfer(alice.address, ONE_TOKEN.mul(100));
+    await token.connect(alice).approve(router.address, ONE_TOKEN.mul(100));
 
     await router.connect(alice).swapExactTokensForTokens(
       utils.parseEther("10"),
@@ -134,11 +134,11 @@ describe("dfERC20", function () {
       alice.address,
       99999999999
     );
-    //console.log("success")
+    //console.log("success");
 
-    let alice_bal = await dtoken.balanceOf(alice.address)
-    //console.log("try to sell")
-    await dtoken.connect(alice).approve(router.address, ONE_TOKEN.mul(8))
+    let alice_bal = await dtoken.balanceOf(alice.address);
+    //console.log("try to sell");
+    await dtoken.connect(alice).approve(router.address, ONE_TOKEN.mul(8));
     await router.connect(alice).swapExactTokensForTokens(
       utils.parseEther("8"),
       0,
@@ -146,11 +146,11 @@ describe("dfERC20", function () {
       alice.address,
       99999999999
     );
-    //console.log("success")
+    //console.log("success");
 
-    alice_bal = alice_bal.sub(await dtoken.balanceOf(alice.address))
+    alice_bal = alice_bal.sub(await dtoken.balanceOf(alice.address));
 
-    expect(alice_bal).to.be.equal(ONE_TOKEN.mul(8))
+    expect(alice_bal).to.be.equal(ONE_TOKEN.mul(8));
 
-  })
-}) 
+  });
+});
